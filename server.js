930ccacf14d24b4907201e82423b5797ba7a5243
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2020-06-22 16:21:36 -0300

    antes de aplicar o nunjucks

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const server = express()
+const nunjucks = require('nunjucks')
+
+server.set("view engine", "html")
+
+// directory when nunjucks will read files
+nunjucks.configure("views", {
+  express: server
+})
+
 
 server.get("/", function(req, res) {
-  return res.send('Hi!')
+  return res.send('Hii!')
 })
 
 server.listen(5000, function() {
