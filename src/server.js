20a52964352d009ffbf4289e02a63f6032d97ee1
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2020-11-16 22:17:14 -0300

    exchange files from html to njk

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ server.use(express.static('public'))
 server.use(methodOverride('_method'))
 server.use(routes)
 
-server.set("view engine", "html")
+server.set("view engine", "njk")
 
 nunjucks.configure("./src/app/views", {
   express: server,
