eb59dbd0ddb16e13e67e2deb158f4b5180c565b1
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2020-12-04 17:51:49 -0300

    att

--- public/script.js
+++ public/script.js
@@ -52,7 +52,7 @@ for(item of navItems) {
 
 /** arquivo admin */
 
-const PhotosUpload = {
+const PhotosUploadTeste = {
   input: "",
   uploadLimit: 5,
   preview: document.querySelector('#photos-preview'),
@@ -171,6 +171,121 @@ const PhotosUpload = {
     photoDiv.remove()
   }
 }
+
+const PhotosUpload = {
+
+  input: "",
+  preview: document.querySelector('#photos-preview1'),
+  uploadLimit: 5,
+  files: [],
+
+  handleFileInput(event) {
+
+    const { files: fileList } = event.target
+    PhotosUpload.input = event.target
+
+    if (PhotosUpload.hasLimit(event)) return
+
+    Array.from(fileList).forEach(file => {
+
+      PhotosUpload.files.push(file)
+
+      const reader = new FileReader()
+
+      reader.onload = () => {
+
+        const image = new Image()
+        image.src = String(reader.result)
+
+        const div = PhotosUpload.getContainer(image)
+
+        PhotosUpload.preview.appendChild(div)
+
+      }
+
+      reader.readAsDataURL(file)
+
+    })
+
+    PhotosUpload.input.files = PhotosUpload.getAllFiles()
+
+
+  },
+  hasLimit(event) {
+
+    const { uploadLimit, input: fileList } = PhotosUpload
+
+    if (fileList.length > uploadLimit) {
+      alert(`Envie no máximo ${uploadLimit} fotos`)
+
+      event.preventDefault()
+
+      return true
+    }
+
+    console.log(PhotosUpload.preview)
+    return false
+
+    
+
+  },
+  getAllFiles() {
+
+    const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer()
+
+    PhotosUpload.files.forEach(file => dataTransfer.items.add(file))
+
+    return dataTransfer.files
+
+  },
+  getContainer(image) {
+
+    const div = document.createElement('div')
+
+    div.classList.add('photo')
+
+    div.onclick = PhotosUpload.removePhoto
+
+    div.appendChild(image)
+
+    div.appendChild(PhotosUpload.getRemoveButton())
+
+    return div
+  },
+  getRemoveButton() {
+
+    const button = document.createElement('i')
+
+    button.classList.add('material-icons')
+
+    button.innerHTML = "delete"
+
+    return button
+
+  },
+  removePhoto(event) {
+
+    const photoDiv = event.target.parentNode // tag i . div class photo
+    console.log(photoDiv)
+    const photosArray = Array.from(PhotosUpload.preview.children)
+
+    console.log(photosArray)
+
+
+    const index = photosArray.indexOf(photoDiv)
+
+    
+
+    PhotosUpload.files.splice(index, 1)
+    PhotosUpload.input.files = PhotosUpload.getAllFiles()
+
+    photoDiv.remove();
+
+  }
+}
+
+
+
 
 const ImageGallery = {
   imagecontainer: document.querySelector('.main-image > img'),
