b14067fcdca665c8d713a72bface850b6aa633db
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2020-12-16 15:42:11 -0300

    add dinamic query to search users

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,23 @@
+const User = require('../models/User')
+
 module.exports = {
   registerForm(req, res) {
     return res.render("user/register")
+  },
+  post(req, res) {
+    //check if has all fields
+    const keys = Object.keys(req.body)
+
+    for (key of keys) {
+      if (req.body[key] == "") {
+        return res.send('Please, fill all fields!')
+      }
+    }
+
+    //check if user exists [email]
+    const { email } = req.body
+    const user = await User.findOne({ where: { email }})
+
+    //check if password match
   }
 }
