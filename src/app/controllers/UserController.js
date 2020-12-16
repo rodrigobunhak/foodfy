63f84ddd5f8cfd6d716405456a12924e71d441aa
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2020-12-16 16:16:54 -0300

    add module validator

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,20 +4,9 @@ module.exports = {
   registerForm(req, res) {
     return res.render("user/register")
   },
-  post(req, res) {
-    //check if has all fields
-    const keys = Object.keys(req.body)
+  async post(req, res) {
+    
 
-    for (key of keys) {
-      if (req.body[key] == "") {
-        return res.send('Please, fill all fields!')
-      }
-    }
-
-    //check if user exists [email]
-    const { email } = req.body
-    const user = await User.findOne({ where: { email }})
-
-    //check if password match
+    return res.send('passed')
   }
 }
