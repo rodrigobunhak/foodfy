1f180997c7579fc84e57586536b9f2b50bf99c5a
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2020-12-23 17:33:12 -0300

    add feature password recovery from users

--- src/routes/users.js
+++ src/routes/users.js
@@ -13,10 +13,10 @@ routes.post('/login',SessionValidator.login, SessionController.login)
 routes.post('/logout', SessionController.logout)
 
 // // reset password / forgot
-// routes.get('/forgot-password', SessionController.forgotForm)
-// routes.get('/password-reset', SessionController.resetForm)
-// routes.post('/forgot-password', SessionController.forgot)
-// routes.post('/password-reset', SessionController.reset)
+routes.get('/forgot-password', SessionController.forgotForm)
+routes.get('/password-reset', SessionController.resetForm)
+routes.post('/forgot-password', SessionValidator.forgot, SessionController.forgot)
+routes.post('/password-reset', SessionValidator.reset, SessionController.reset)
 
 // // user register UserController
 routes.get('/register', UserController.registerForm)
